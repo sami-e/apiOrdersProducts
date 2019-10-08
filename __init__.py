3865ea64b7cd285047f798a6fe5b4057dacc154d
Sami EL KHAIRA <sami.elkhaira@gmail.com>
2019-10-08 00:06:25 -0400

    Adding errors handling to POST order

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, abort, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, Response
 from inf5190 import models
 from inf5190 import views
 from inf5190 import services
@@ -11,15 +10,19 @@ def create_app(initial_config=None):
     
     @app.route("/")
     def index():
-        return services.ProductServices.get_products()
-
+        products = services.ProductServices.get_products()
+        return products.response, products.status, products.headers
+    
     @app.route("/order", methods=["POST"])
     def create_order():
         order = services.OrderServices.new_order(request.json)
-        return redirect(url_for("get_order", order_id=order.id))
+        if order.status == "422 UNPROCESSABLE ENTITY":
+            return order.response, order.status, order.headers
+        return redirect(url_for("get_order", order_id=order.response)), order.status
     
     @app.route("/order/<int:order_id>", methods=["GET"])
     def get_order(order_id):
-        return services.OrderServices.formatted_order(order_id)
+        order = services.OrderServices.formatted_order(order_id)
+        return order.response, order.status, order.headers
     
     return app
--- services.py
+++ services.py
@@ -1,5 +1,5 @@
 import json
-from flask import abort
+from flask import abort, Response, jsonify
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
 from inf5190 import models
@@ -11,13 +11,14 @@ class ProductServices:
     @classmethod
     def get_products(cls):
         db_products = models.Product.select()
-        product_list = []
+        products_list = []
         for db_product in db_products:
             product = {"description": db_product.description, "id": db_product.id, "image": db_product.image,
                        "in_stock": db_product.in_stock, "name": db_product.name, "price": db_product.price,
                        "weight": db_product.weight}
-            product_list.append(product)
-        return {"products": product_list}
+            products_list.append(product)
+        return Response(response=jsonify({"products": products_list}), status=200,
+                        headers={"Content-Type": "application/json; charset=utf-8"})
 
 
 class OrderServices:
@@ -35,23 +36,57 @@ class OrderServices:
                       "credit_card": credit_card, "shipping_information": shipping_information,
                       "paid": paid, "transaction": transaction, "product": product,
                       "shipping_price": order.shipping_price}
-        return {"order": order_dict}
+        return Response(response=jsonify({"order": order_dict}), status=200,
+                        headers={"Content-Type": "application/json; charset=utf-8"})
     
     @classmethod
     def new_order(cls, post_data):
-        product_id = post_data["product"]["id"]
-        quantity = post_data["product"]["quantity"]
-        product = models.Product.get_or_none(models.Product.id == product_id)
-        price = product.price
-        weight = product.weight
-        total_price = price * quantity
-        if weight < 500.0:
-            shipping_price = 5
-        elif weight < 2000.0:
-            shipping_price = 10
+        if "product" in post_data and "id" in post_data["product"] and "quantity" in post_data["product"] \
+                and post_data["product"] and post_data["product"]["id"] and post_data["product"]["quantity"] \
+                and post_data["product"]["quantity"] > 0:
+            
+            product_id = post_data["product"]["id"]
+            quantity = post_data["product"]["quantity"]
+            product = models.Product.get_or_none(models.Product.id == product_id)
+            
+            if not product or not product.in_stock:
+                error_stock = {
+                    "errors": {
+                        "product": {
+                            "code": "out-of-inventory",
+                            "name": "Le produit demandé n'est pas en inventaire"
+                        }
+                    }
+                }
+                return Response(response=jsonify(error_stock), status=422,
+                                headers={"Content-Type": "application/json; charset=utf-8"})
+            
+            price = product.price
+            weight = product.weight
+            total_price = price * quantity
+            if weight < 500.0:
+                shipping_price = 5
+            elif weight < 2000.0:
+                shipping_price = 10
+            else:
+                shipping_price = 25
+
+            order = models.Order.create(product_id=product_id, quantity=quantity, total_price=total_price,
+                                        shipping_price=shipping_price, paid=False)
+            order_id = order.id
+            return Response(response=order_id, status=302)
+        
         else:
-            shipping_price = 25
-        return models.Order.create(product_id=product_id, quantity=quantity, total_price=total_price, shipping_price=shipping_price)
+            error_missing = {
+                "errors": {
+                    "product": {
+                        "code": "missing-fields",
+                        "name": "La création d'une commande nécessite un produit"
+                    }
+                }
+            }
+            return Response(response=jsonify(error_missing), status=422,
+                            headers={"Content-Type": "application/json; charset=utf-8"})
 
 
 class ApiError(Exception):
