cb74ce5f20729aff7d85f4ac25a2f2144eab1ee5
khoury-pierre <khoury_pierre@hotmail.com>
2019-10-11 18:09:36 -0400

    update shiping information

--- __init__.py
+++ __init__.py
@@ -7,23 +7,27 @@ from inf5190.services import init_app
 def create_app(initial_config=None):
     app = Flask("inf5190")
     init_app(app)
-    
+
     @app.route("/", methods=["GET"])
     def index():
         products = ProductController.get_products()
         return products.response, products.status, products.headers
-    
+
     @app.route("/order", methods=["POST"])
     def create_order():
         order = OrderController.new_order(request.json)
         if order.status == "422 UNPROCESSABLE ENTITY":
             return order.response, order.status, order.headers
         return redirect(url_for("get_order", order_id=order.response)), order.status
-    
+
     @app.route("/order/<int:order_id>", methods=["GET"])
     def get_order(order_id):
         order = OrderController.formatted_order(order_id)
         return order.response, order.status, order.headers
-    
-    return app
 
+    @app.route("/order/<int:order_id>", methods=["PUT"])
+    def put_order(order_id):
+        order = OrderController.UpDateOrder(request.json, order_id)
+        return order.response, order.status, order.headers
+
+    return app
--- controller/orderController.py
+++ controller/orderController.py
@@ -20,17 +20,17 @@ class OrderController:
                       "shipping_price": order.shipping_price}
         return Response(response=jsonify({"order": order_dict}), status=200,
                         headers={"Content-Type": "application/json; charset=utf-8"})
-    
+
     @classmethod
     def new_order(cls, post_data):
         if "product" in post_data and "id" in post_data["product"] and "quantity" in post_data["product"] \
                 and isinstance(post_data["product"]["id"], int) and isinstance(post_data["product"]["quantity"], int) \
                 and post_data["product"]["quantity"] > 0:
-            
+
             product_id = post_data["product"]["id"]
             quantity = post_data["product"]["quantity"]
             product = Product.get_or_none(Product.id == product_id)
-            
+
             if not product or not product.in_stock:
                 error_stock = {
                     "errors": {
@@ -42,7 +42,7 @@ class OrderController:
                 }
                 return Response(response=jsonify(error_stock), status=422,
                                 headers={"Content-Type": "application/json; charset=utf-8"})
-            
+
             price = product.price
             weight = product.weight
             total_price = price * quantity
@@ -52,12 +52,12 @@ class OrderController:
                 shipping_price = 10
             else:
                 shipping_price = 25
-            
+
             order = Order.create(product_id=product_id, quantity=quantity, total_price=total_price,
                                  shipping_price=shipping_price, paid=False)
             order_id = order.id
             return Response(response=order_id, status=302)
-        
+
         else:
             error_missing = {
                 "errors": {
@@ -67,5 +67,39 @@ class OrderController:
                     }
                 }
             }
+            return Response(response=jsonify(error_missing), status=422,
+
+                            headers={"Content-Type": "application/json; charset=utf-8"})
+
+        @classmethod
+        def UpDateOrder(cls, post_data, order_id):
+
+            if "order" in post_data and "email" in post_data["order"] \
+                    and "country" in post_data["order"]["shipping_information"] and "address" in post_data["order"][
+                "shipping_information"] \
+                    and "postal_code" in post_data["order"]["shipping_information"] and "city" in post_data["order"][
+                "shipping_information"] \
+                    and "province" in post_data["order"]["shipping_information"]:
+                email = post_data["order"]["email"]
+                country = post_data["order"]["shipping_information"]["country"]
+                address = post_data["order"]["shipping_information"]["address"]
+                postal_code = post_data["order"]["shipping_information"]["postal_code"]
+                city = post_data["order"]["shipping_information"]["city"]
+                province = post_data["order"]["shipping_information"]["province"]
+
+                Order.PUT(email=email)
+                ShippingInformation.PUT(country=country, address=address, postal_code=postal_code, \
+                                        city=city, province=province)
+                return Response(response=order_id, status=302)
+
+            else:
+                error_missing = {
+                    "errors": {
+                        "order": {
+                            "code": "missing-fields",
+                            "name": "Il manque un ou plusieurs champs qui sont obligatoire"
+                        }
+                    }
+                }
             return Response(response=jsonify(error_missing), status=422,
                             headers={"Content-Type": "application/json; charset=utf-8"})
