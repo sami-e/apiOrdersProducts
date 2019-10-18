2ba431468118c8b7cc25ebece87df04a1d1aebfd
Sami EL KHAIRA <sami.elkhaira@gmail.com>
2019-10-18 19:49:22 -0400

    Done: update credit card

--- controller/orderController.py
+++ controller/orderController.py
@@ -1,6 +1,6 @@
 from flask import abort, Response, jsonify
 
-from inf5190.services import perform_request
+from inf5190.services import perform_request, ApiError
 from inf5190.model.productModel import Product
 from inf5190.model.orderModel import Order
 from inf5190.model.shippingInfoModel import ShippingInformation
@@ -20,7 +20,6 @@ class OrderController:
         if not order.credit_card:
             credit_card = {}
             transaction = {}
-            paid = False
         else:
             credit_card = {
                 "name": order.credit_card.name,
@@ -34,7 +33,6 @@ class OrderController:
                 "success": order.transaction.success,
                 "amount_charged": order.transaction.amount_charged
             }
-            paid = True
     
         if not order.shipping_information:
             shipping_information = {}
@@ -49,7 +47,7 @@ class OrderController:
 
         order_dict = {"id": order.id, "total_price": order.total_price, "email": order.email,
                       "credit_card": credit_card, "shipping_information": shipping_information,
-                      "paid": paid, "transaction": transaction, "product": product,
+                      "paid": order.paid, "transaction": transaction, "product": product,
                       "shipping_price": order.shipping_price}
         return Response(response=jsonify({"order": order_dict}), status=200,
                         headers={"Content-Type": "application/json; charset=utf-8"})
@@ -105,9 +103,8 @@ class OrderController:
 
     @classmethod
     def update_order(cls, post_data, order_id):
-        order = Order.get_or_none(Order.id == order_id)
-        if order.email:
-            cls.update_credit_card(post_data, order_id)
+        if "credit_card" in post_data:
+            return cls.update_credit_card(post_data, order_id)
         else:
             if "order" in post_data and "email" in post_data["order"] and "shipping_information" in post_data["order"] \
                     and "country" in post_data["order"]["shipping_information"] \
@@ -115,6 +112,7 @@ class OrderController:
                     and "postal_code" in post_data["order"]["shipping_information"] \
                     and "city" in post_data["order"]["shipping_information"] \
                     and "province" in post_data["order"]["shipping_information"]:
+                
                 email = post_data["order"]["email"]
                 country = post_data["order"]["shipping_information"]["country"]
                 address = post_data["order"]["shipping_information"]["address"]
@@ -143,21 +141,23 @@ class OrderController:
     @staticmethod
     def update_credit_card(post_data, order_id):
         order = Order.get_or_none(Order.id == order_id)
-        if order.paid:
-            error_already_paid = {
-                "errors": {
-                    "order": {
-                        "code": "already-paid",
-                        "name": "La commande a déjà été payée."
-                    }
-                }
-            }
-            return Response(response=jsonify(error_already_paid), status=422,
-                            headers={"Content-Type": "application/json; charset=utf-8"})
         
         if "credit_card" in post_data and "name" in post_data["credit_card"] and "number" in post_data["credit_card"] \
                 and "expiration_year" in post_data["credit_card"] and "cvv" in post_data["credit_card"] \
                 and "expiration_month" in post_data["credit_card"]:
+            
+            if order.paid:
+                error_already_paid = {
+                    "errors": {
+                        "order": {
+                            "code": "already-paid",
+                            "name": "La commande a déjà été payée."
+                        }
+                    }
+                }
+                return Response(response=jsonify(error_already_paid), status=422,
+                                headers={"Content-Type": "application/json; charset=utf-8"})
+            
             name = post_data["credit_card"]["name"]
             number = post_data["credit_card"]["number"]
             expiration_month = post_data["credit_card"]["expiration_month"]
@@ -172,31 +172,23 @@ class OrderController:
                                 "expiration_month": expiration_month
                                 },
                             "amount_charged": order.total_price+order.shipping_price}
-            payment_response = perform_request(uri="pay", method="POST", data=payment_data)[0]
+            try:
+                payment_response = perform_request(uri="pay", method="POST", data=payment_data)[0]
+            except ApiError as e:
+                return Response(response=e.content, status=e.code,
+                                headers={"Content-Type": "application/json; charset=utf-8"})
             
-            if payment_response:
-                credit_card = CreditCard.create(name=payment_response["credit_card"]["name"],
-                                                first_digits=payment_response["credit_card"]["first_digits"],
-                                                last_digits=payment_response["credit_card"]["last_digits"],
-                                                expiration_year=payment_response["credit_card"]["expiration_year"],
-                                                expiration_month=payment_response["credit_card"]["expiration_month"])
-                transaction = Transaction.create(id=payment_response["transaction"]["id"],
-                                                 success=payment_response["transaction"]["success"],
-                                                 amount_charged=payment_response["transaction"]["amount_charged"])
-                Order.update(credit_card=credit_card.id,
-                             transaction=transaction.id).where(Order.id == order_id).execute()
-                return Response(status=200)
-            else:
-                error_declined = {
-                    "errors": {
-                        "credit_card": {
-                            "code": "card-declined",
-                            "name": "La carte de crédit a été déclinée"
-                        }
-                    }
-                }
-            return Response(response=jsonify(error_declined), status=422,
-                            headers={"Content-Type": "application/json; charset=utf-8"})
+            credit_card = CreditCard.create(name=payment_response["credit_card"]["name"],
+                                            first_digits=payment_response["credit_card"]["first_digits"],
+                                            last_digits=payment_response["credit_card"]["last_digits"],
+                                            expiration_year=payment_response["credit_card"]["expiration_year"],
+                                            expiration_month=payment_response["credit_card"]["expiration_month"])
+            transaction = Transaction.create(id=payment_response["transaction"]["id"],
+                                             success=payment_response["transaction"]["success"],
+                                             amount_charged=payment_response["transaction"]["amount_charged"])
+            Order.update(credit_card=credit_card.id,
+                         transaction=transaction.id, paid=True).where(Order.id == order_id).execute()
+            return Response(status=200)
         
         else:
             error_missing = {
--- model/orderModel.py
+++ model/orderModel.py
@@ -3,13 +3,13 @@ from inf5190.model import models
 from inf5190.model.productModel import Product
 from inf5190.model.shippingInfoModel import ShippingInformation
 from inf5190.model.transactionModel import Transaction
-from peewee import CharField, ForeignKeyField, FloatField, IntegerField, BooleanField
+from peewee import CharField, ForeignKeyField, IntegerField, BooleanField
 
 
 class Order(models.BaseModel):
     product = ForeignKeyField(Product, backref="orders")
     quantity = IntegerField()
-    total_price = FloatField()
+    total_price = IntegerField()
     email = CharField(null=True)
     credit_card = ForeignKeyField(CreditCard, backref="orders", null=True)
     shipping_information = ForeignKeyField(ShippingInformation, backref="orders", null=True)
