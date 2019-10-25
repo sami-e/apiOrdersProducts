4d7324496f464c328be5795fa25389881db08903
khoury-pierre <khoury_pierre@hotmail.com>
2019-10-25 18:28:46 -0400

    test modification2

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,17 +1,16 @@
 import json
-from urllib.request import Request, urlopen
 from flask import Request, json
-
 from inf5190.services import perform_request
 
 
 class TestApiError(object):
     def test_perform_request(self):
         reponse_data = perform_request("products")
-        data = json.loads(reponse_data)
-        assert data is not None
-        assert b"1248" in data
-        assert b"1232" in data
-        assert b"1235" in data
-        assert b"1245" in data
-        assert b"1231" in data
+        data_str = json.dumps(reponse_data)
+        assert data_str is not None
+        assert "1248" in data_str
+        assert "1232" in data_str
+        assert "1235" in data_str
+        assert "1245" in data_str
+        assert "1231" in data_str
+
