d3ca067a25134c44cab4f590bd4e99763100b533
khoury-pierre <khoury_pierre@hotmail.com>
2019-10-26 20:04:25 -0400

    test integration2

--- test/test_int.py
+++ test/test_int.py
@@ -1,38 +1,3 @@
-#     def test_load_from_file(self):
-#         #TODO : test qui assure que le fichier JSON est lu et que la classe est instanciée
-#         address_book = AddressBook.load_from_file(FILENAME)
-#         assert address_book is not None
-#         assert len(address_book.addresses) == 1
-#         pass
-#
-# class TestApp(object):
-#     def test_index(self):
-#         #TODO : rajouter un test qui assure que l'index retourne un 200 avec le nom des personnes du carnet d'adresse
-#         client = app.test_client()
-#         response = client.get("/")
-#         assert response.status_code == 200
-#         assert b"Gaston Lagaffe" in response.data
-#         pass
-#
-#     def test_address(self):
-#         client = app.test_client()
-#
-#         response = client.get("/0")
-#         assert response.status_code == 200
-#         assert b"Gaston Lagaffe" in response.data
-#
-#     def test_address_does_not_exist(self):
-#         client = app.test_client()
-#
-#         response = client.get("/999")
-#         assert response.status_code == 404
-#
-#     def test_new_address(self):
-#         client = app.test_client()
-#
-#         response = client.get("/new")
-#         assert response.status_code == 200
-
 import pytest
 
 order_post = {
@@ -41,7 +6,27 @@ order_post = {
         "quantity": 2
     }
 }
-
+shipping_info = {
+    "order": {
+        "email": "caissy.jean-philippe@uqam.ca",
+        "shipping_information": {
+            "country": "Canada",
+            "address": "201, rue Président-Kennedy",
+            "postal_code": "H2X 3Y7",
+            "city": "Montréal",
+            "province": "QC"
+        }
+    }
+}
+credit_card = {
+    "credit_card": {
+        "name": "John Doe",
+        "number": "4242 4242 4242 4242",
+        "expiration_year": 2024,
+        "cvv": "123",
+        "expiration_month": 9
+    }
+}
 
 class TestPoll(object):
     def test_create_Order(self, app, client):
@@ -62,12 +47,10 @@ class TestPoll(object):
             response = client.get("/order/1")
             assert response.status_code == 200
             assert b"1245" in response.data
+            response = client.put("/order/1", json=shipping_info)
+            assert response.status_code == 200
+            assert b"email" in response.data
 
-            #
-            # response = client.post("/polls/new", data={"name": "Mon premier sondage"})
-            # assert response.status_code == 302
-            # assert response.location == "http://localhost/polls/1"
-            #
-            # response = client.get("/polls/1")
-            # assert response.status_code == 200
-            # assert b'Mon premier sondage' in response.data
+            response = client.put("/order/1", json=credit_card)
+            assert response.status_code == 200
+            assert b"success" in response.data
