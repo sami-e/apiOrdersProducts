0df85d654b66531cb320b494c516df92ac60aa2a
Sami EL KHAIRA <sami.elkhaira@gmail.com>
2019-11-01 17:07:07 -0400

    Moving CODES-PERMANENTS to root + merging unit test with func tests

--- tests/test_fonc.py
+++ tests/test_fonc.py
@@ -1,7 +1,19 @@
+import json
 from tests.test_data import order, order_false, shipping_info, credit_card
+from inf5190.services import perform_request
 
 
 class TestRoutes(object):
+    
+    def test_perform_request(self):
+        response_data = perform_request("products")
+        data_str = json.dumps(response_data)
+        assert data_str is not None
+        assert "1248" in data_str
+        assert "1232" in data_str
+        assert "1235" in data_str
+        assert "1245" in data_str
+        assert "1231" in data_str
 
     def test_index(self, app, client):
         with app.app_context():
