bb8d712ee1f604bbd9104605eb67786abb1fc46a
Mohammed Sami EL KHAIRA <sami.elkhaira@gmail.com>
2019-11-24 11:45:12 -0500

    Revert "Fin TP1"
    This reverts commit e40d7329ede69fbc8ff0b8033433b43befa694a7.

--- inf5190/controller/orderController.py
+++ inf5190/controller/orderController.py
@@ -1,7 +1,3 @@
-import os
-import json
-from playhouse.shortcuts import model_to_dict, dict_to_model
-from redis import Redis
 from inf5190.services import perform_request, ApiError
 from inf5190.model.productModel import Product
 from inf5190.model.orderModel import Order
@@ -10,42 +6,26 @@ from inf5190.model.creditCardModel import CreditCard
 from inf5190.model.transactionModel import Transaction
 from inf5190.view import views
 
-redis_url = os.environ.get('REDIS_URL', 'redis://localhost')
-redis_conn = Redis.from_url(redis_url)
-
-
 
 class OrderController:
     @classmethod
     def formatted_order(cls, order_id):
-        cle = f"order-{order_id}"
-        order_cached = redis_conn.get(cle)
-        if order_cached:
-            order_cached = dict_to_model(Order, json.loads(order_cached))
-            return views.display_order(order_cached)
-
         order = Order.get_or_none(Order.id == order_id)
         return views.display_order(order)
-
+    
     @classmethod
     def create_order(cls, post_data):
-        total_price = 0
-        shipping_price = 0
-        order_r = 0
-        i = 0
-        for rows in post_data["products"]:
-
-            # "product" in post_data and "id" in rows.post_data["product"] and "quantity" in rows.post_data["product"] \
-            # and isinstance(rows.post_data["product"]["id"], int) and \
-            # isinstance(rows.post_data["product"]["quantity"], int) \
-            # and rows.post_data["product"]["quantity"] > 0:
-            product_id = rows["id"]
-            quantity = rows["quantity"]
+        if "product" in post_data and "id" in post_data["product"] and "quantity" in post_data["product"] \
+                and isinstance(post_data["product"]["id"], int) and isinstance(post_data["product"]["quantity"], int) \
+                and post_data["product"]["quantity"] > 0:
+            
+            product_id = post_data["product"]["id"]
+            quantity = post_data["product"]["quantity"]
             product = Product.get_or_none(Product.id == product_id)
-
+            
             if not product or not product.in_stock:
                 return views.display_error_out_of_inventory()
-
+            
             price = product.price
             weight = product.weight
             total_price = price * quantity
@@ -55,22 +35,14 @@ class OrderController:
                 shipping_price = 10
             else:
                 shipping_price = 25
-            if i == 0:
-                i == 1
-                order = Order.create(product_id=product_id, quantity=quantity, total_price=total_price,
-                                     shipping_price=shipping_price, paid=False)
-                total_price = Order.total_price
-                shipping_price = shipping_price
-                order_r = order.id
-            elif i == 1:
-                order = Order.update(product_id=product_id, quantity=quantity,
-                                     total_price=Order.total_price + total_price,
-                                     shipping_price=Order.shipping_price + shipping_price, paid=False)
-                total_price = Order.total_price + total_price
-                shipping_price = Order.shipping_price + shipping_price
-
-        return views.display_post_redirect(order_r)
-
+            
+            order = Order.create(product_id=product_id, quantity=quantity, total_price=total_price,
+                                 shipping_price=shipping_price, paid=False)
+            return views.display_post_redirect(order.id)
+        
+        else:
+            return views.display_error_missing_fields_product()
+    
     @classmethod
     def update_order(cls, post_data, order_id):
         if "credit_card" in post_data:
@@ -86,7 +58,7 @@ class OrderController:
                 and "postal_code" in post_data["order"]["shipping_information"] \
                 and "city" in post_data["order"]["shipping_information"] \
                 and "province" in post_data["order"]["shipping_information"]:
-
+        
             email = post_data["order"]["email"]
             country = post_data["order"]["shipping_information"]["country"]
             address = post_data["order"]["shipping_information"]["address"]
@@ -99,30 +71,30 @@ class OrderController:
             Order.update(email=email,
                          shipping_information=shipping_information.id).where(Order.id == order_id).execute()
             return views.display_ok()
-
+    
         else:
             return views.display_error_missing_fields_order()
-
+    
     @staticmethod
     def update_credit_card(post_data, order_id):
         order = Order.get_or_none(Order.id == order_id)
-
+        
         if not order.shipping_information:
             return views.display_error_missing_shipping_info()
-
+        
         if "credit_card" in post_data and "name" in post_data["credit_card"] and "number" in post_data["credit_card"] \
                 and "expiration_year" in post_data["credit_card"] and "cvv" in post_data["credit_card"] \
                 and "expiration_month" in post_data["credit_card"]:
-
+            
             if order.paid:
                 return views.display_error_already_paid()
-
+            
             name = post_data["credit_card"]["name"]
             number = post_data["credit_card"]["number"]
             expiration_month = post_data["credit_card"]["expiration_month"]
             expiration_year = post_data["credit_card"]["expiration_year"]
             cvv = post_data["credit_card"]["cvv"]
-
+            
             payment_data = {"credit_card": {
                 "name": name,
                 "number": number,
@@ -131,12 +103,12 @@ class OrderController:
                 "expiration_month": expiration_month
             },
                 "amount_charged": order.total_price + order.shipping_price}
-
+            
             try:
                 payment_response = perform_request(uri="pay", method="POST", data=payment_data)
             except ApiError as error:
                 return views.display_error_payment_api(error)
-
+            
             credit_card = CreditCard.create(name=payment_response["credit_card"]["name"],
                                             first_digits=payment_response["credit_card"]["first_digits"],
                                             last_digits=payment_response["credit_card"]["last_digits"],
@@ -147,11 +119,7 @@ class OrderController:
                                              amount_charged=payment_response["transaction"]["amount_charged"])
             Order.update(credit_card=credit_card.id,
                          transaction=transaction.id, paid=True).where(Order.id == order_id).execute()
-
-            order = Order.get_or_none(Order.id == order_id)
-            order_cached = json.dumps(model_to_dict(order))
-            redis_conn.set(f"order-{order_id}", order_cached)
             return views.display_ok()
-
+        
         else:
             return views.display_error_missing_fields_order()
